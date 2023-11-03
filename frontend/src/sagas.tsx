f37b0ebc28dd0fcec282fcfe9c2a869572b91fe4
Jason Gin <jasongin88@gmail.com>
2023-11-03 18:07:37 -0400

    fix: remove deprecated sagas

--- frontend/src/sagas.tsx
+++ frontend/src/sagas.tsx
@@ -1,16 +1,8 @@
 import { all } from 'redux-saga/effects';
 import recipeGridSaga from './features/recipegrid/recipegridSaga';
 import recipeSaga from './features/recipepage/recipeSaga';
-import toolsSaga from './features/tools/toolsSaga';
-import ingredientsSaga from './features/ingredients/ingredientsSaga';
 import adminSaga from './features/admin/adminSaga';
 
 export default function* rootSaga() {
-  yield all([
-    recipeGridSaga(),
-    recipeSaga(),
-    toolsSaga(),
-    ingredientsSaga(),
-    adminSaga(),
-  ]);
+  yield all([recipeGridSaga(), recipeSaga(), adminSaga()]);
 }
