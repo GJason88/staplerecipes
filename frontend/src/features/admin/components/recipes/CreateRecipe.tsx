4534dc85aaea2ca5c7870ce9438e548f4cd4bc9d
Jason Gin <jasongin88@gmail.com>
2023-11-13 16:36:35 -0500

    refactor: abstract recipe form

--- frontend/src/features/admin/components/recipes/CreateRecipe.tsx
+++ frontend/src/features/admin/components/recipes/CreateRecipe.tsx
@@ -1,42 +1,11 @@
-import { Button, FormControl, Stack } from '@mui/material';
-import { recipeWidth } from '../../../../data/constants';
-import { useDispatch, useSelector } from 'react-redux';
-import { IRootState } from '../../../..';
-import EditRecipeInfo from './components/RecipeInfoField';
-import EditRecipeIngredients from './components/RecipeIngredientsField';
-import EditRecipeTools from './components/RecipeToolsField';
-import EditRecipeInstructions from './components/RecipeInstructionsField';
 import { createNewRecipeRequest } from './adminRecipesReducer';
+import RecipeForm from './components/RecipeForm';
 
 export default function CreateRecipe() {
-  const dispatch = useDispatch();
-  const recipe = useSelector<IRootState, RecipeState>((state) => state.recipe);
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(createNewRecipeRequest(recipe));
-  };
   return (
-    <form onSubmit={onSubmit}>
-      <FormControl fullWidth>
-        <Stack gap={3} alignItems='center'>
-          <EditRecipeInfo
-            recipeName={recipe.recipeName}
-            time={recipe.time}
-            diet={recipe.diet}
-            servings={recipe.servings}
-          />
-          <EditRecipeIngredients recipeIngredients={recipe.ingredients} />
-          <EditRecipeTools recipeTools={recipe.tools} />
-          <EditRecipeInstructions instructions={recipe.instructions} />
-          <Button
-            type='submit'
-            variant='contained'
-            sx={{ width: recipeWidth, mb: 3 }}
-          >
-            Create Recipe
-          </Button>
-        </Stack>
-      </FormControl>
-    </form>
+    <RecipeForm
+      submitBtnText='Create Recipe'
+      submitAction={createNewRecipeRequest}
+    />
   );
 }
---
+++ frontend/src/features/admin/components/recipes/components/RecipeForm.tsx
@@ -0,0 +1,50 @@
+import { FormControl, Stack, Button } from '@mui/material';
+import { recipeWidth } from '../../../../../data/constants';
+import EditRecipeInfo from './RecipeInfoField';
+import EditRecipeIngredients from './RecipeIngredientsField';
+import EditRecipeInstructions from './RecipeInstructionsField';
+import EditRecipeTools from './RecipeToolsField';
+import { useDispatch, useSelector } from 'react-redux';
+import { IRootState } from '../../../../..';
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
+
+interface RecipeFormProps {
+  submitBtnText: string;
+  submitAction: ActionCreatorWithPayload<any, string>;
+}
+
+export default function RecipeForm({
+  submitBtnText,
+  submitAction,
+}: RecipeFormProps) {
+  const dispatch = useDispatch();
+  const recipe = useSelector<IRootState, RecipeState>((state) => state.recipe);
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(submitAction(recipe));
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <FormControl fullWidth>
+        <Stack gap={3} alignItems='center'>
+          <EditRecipeInfo
+            recipeName={recipe.recipeName}
+            time={recipe.time}
+            diet={recipe.diet}
+            servings={recipe.servings}
+          />
+          <EditRecipeIngredients recipeIngredients={recipe.ingredients} />
+          <EditRecipeTools recipeTools={recipe.tools} />
+          <EditRecipeInstructions instructions={recipe.instructions} />
+          <Button
+            type='submit'
+            variant='contained'
+            sx={{ width: recipeWidth, mb: 3 }}
+          >
+            {submitBtnText}
+          </Button>
+        </Stack>
+      </FormControl>
+    </form>
+  );
+}
