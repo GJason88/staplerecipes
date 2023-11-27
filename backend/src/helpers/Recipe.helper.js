a6d193a0e34a04f8192d30bddb32f85e3d098deb
Jason Gin <jasongin88@gmail.com>
2023-11-27 14:28:51 -0500

    refactor: abstract getRecipe, getRecipes queries

--- backend/src/helpers/Recipe.helper.js
+++ backend/src/helpers/Recipe.helper.js
@@ -1,3 +1,24 @@
+import {
+    additionalMeasurementsQuery,
+    nestSelectQuery,
+    nutrientsSelectQuery,
+} from './utils/nestedSelectQueries.js';
+
+const queries = {
+    recipeIngredientsQuery: `coalesce(${nestSelectQuery(
+        'array_to_json(array_agg(x))',
+        `SELECT i.ingredient_id,ingredient_name,amount,default_measurement,g_ml AS ml_for_100g,${nutrientsSelectQuery},${additionalMeasurementsQuery} 
+         FROM recipes.ingredient AS i
+         INNER JOIN recipes.recipe_ingredient as ri ON i.ingredient_id = ri.ingredient_id AND r.recipe_id = ri.recipe_id`
+    )}, '[]'::json) AS ingredients`,
+    recipeToolsQuery: `coalesce(${nestSelectQuery(
+        'array_to_json(array_agg(x))',
+        `SELECT t.tool_id,tool_name 
+         FROM recipes.tool as t
+         INNER JOIN recipes.recipe_tool as rt ON t.tool_id = rt.tool_id AND r.recipe_id = rt.recipe_id`
+    )}, '[]'::json) AS tools`,
+};
+
 export const recipeHelpers = {
     validateInfo: (info) =>
         typeof info.recipeName === 'string' &&
@@ -10,4 +31,6 @@ export const recipeHelpers = {
         typeof info.time === 'string' &&
         Array.isArray(info.instructions) &&
         info.instructions,
+    getRecipesQuery: `SELECT r.recipe_id,recipe_name,time,diet,servings,instructions,${queries.recipeIngredientsQuery},${queries.recipeToolsQuery} 
+                   FROM recipes.recipe as r`,
 };
--- backend/src/models/Recipe.model.js
+++ backend/src/models/Recipe.model.js
@@ -6,40 +6,16 @@ import {
     nestSelectQuery,
 } from '../helpers/utils/nestedSelectQueries.js';
 import { pgpHelpers } from '../helpers/utils/pgpHelpers.js';
+import { recipeHelpers } from '../helpers/Recipe.helper.js';
 
 const pgp = pgPromise({ capSQL: true });
 
 export const recipeModel = {
-    getRecipes: async () => {
-        const ingredientsQuery = `coalesce(${nestSelectQuery(
-            'array_to_json(array_agg(x))',
-            `SELECT i.ingredient_id,ingredient_name,amount,default_measurement,g_ml AS ml_for_100g,${nutrientsSelectQuery},${additionalMeasurementsQuery} 
-             FROM recipes.ingredient AS i
-             INNER JOIN recipes.recipe_ingredient as ri ON i.ingredient_id = ri.ingredient_id AND r.recipe_id = ri.recipe_id`
-        )}, '[]'::json) AS ingredients`;
-        const toolsQuery = `coalesce(${nestSelectQuery(
-            'array_to_json(array_agg(x))',
-            `SELECT t.tool_id,tool_name 
-             FROM recipes.tool as t
-             INNER JOIN recipes.recipe_tool as rt ON t.tool_id = rt.tool_id AND r.recipe_id = rt.recipe_id`
-        )}, '[]'::json) AS tools`;
-        const recipesQuery = `SELECT r.recipe_id,recipe_name,time,diet,servings,instructions,${ingredientsQuery},${toolsQuery} FROM recipes.recipe as r;`;
-        return await db.any(recipesQuery);
-    },
-    getRecipe: async (recipeId) => {
-        // TODO: use same format as getRecipes
-        const recipeQuery =
-            'SELECT recipe_id,recipe_name,time,diet,servings,instructions FROM recipes.recipe WHERE recipe_id = $1;';
-        const ingredientsQuery = `SELECT i.ingredient_id,ingredient_name,amount,default_measurement,g_ml AS ml_for_100g,${nutrientsSelectQuery},${additionalMeasurementsQuery}
-            FROM recipes.ingredient AS i \
-            INNER JOIN recipes.recipe_ingredient AS r_i ON i.ingredient_id = r_i.ingredient_id AND r_i.recipe_id = $1;`;
-        const toolsQuery =
-            'SELECT t.tool_id,tool_name FROM recipes.tool AS t \
-            INNER JOIN recipes.recipe_tool AS rt ON t.tool_id = rt.tool_id AND rt.recipe_id = $1;';
-        return await db.multi(recipeQuery + ingredientsQuery + toolsQuery, [
+    getRecipes: async () => await db.any(`${recipeHelpers.getRecipesQuery};`),
+    getRecipe: async (recipeId) =>
+        await db.one(`${recipeHelpers.getRecipesQuery} WHERE recipe_id = $1;`, [
             recipeId,
-        ]);
-    },
+        ]),
     createRecipe: async (info) => {
         const recipeQuery = info.recipeId
             ? 'INSERT INTO recipes.recipe(recipe_id, recipe_name, time, diet, servings, instructions) VALUES ($1, $2, $3, $4, $5, $6) RETURNING recipe_id;'
@@ -69,16 +45,17 @@ export const recipeModel = {
                 pgpHelpers.table('recipe_ingredient', 'recipes')
             )
         );
-        info.tools.length && await db.none(
-            pgp.helpers.insert(
-                info.tools.map((tool) => ({
-                    tool_id: tool.toolId,
-                    recipe_id: recipeId,
-                })),
-                pgpHelpers.columns(['tool_id', 'recipe_id']),
-                pgpHelpers.table('recipe_tool', 'recipes')
-            )
-        );
+        info.tools.length &&
+            (await db.none(
+                pgp.helpers.insert(
+                    info.tools.map((tool) => ({
+                        tool_id: tool.toolId,
+                        recipe_id: recipeId,
+                    })),
+                    pgpHelpers.columns(['tool_id', 'recipe_id']),
+                    pgpHelpers.table('recipe_tool', 'recipes')
+                )
+            ));
     },
     deleteRecipe: async (recipeId) =>
         await db.none('DELETE FROM recipes.recipe WHERE recipe_id = $1;', [
