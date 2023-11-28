54d49fec6233cf02fcce0ebaae3a46a8cc9e594d
Jason Gin <jasongin88@gmail.com>
2023-11-28 17:46:36 -0500

    fix: bugs with stale recipe data

--- frontend/src/pages/Recipe.tsx
+++ frontend/src/pages/Recipe.tsx
@@ -5,11 +5,19 @@ import { routes } from '../data/constants';
 import RecipePage from '../features/recipepage/RecipePage';
 import useRecipe from '../hooks/useRecipe';
 import { useParams } from 'react-router-dom';
+import { useQueryClient } from 'react-query';
 
 export default function Recipe() {
   const routeParams = useParams();
   const dispatch = useDispatch();
   const recipe = useRecipe(routeParams.id ?? '');
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    return () => {
+      queryClient.removeQueries(['recipe']); // prevent previous rendered recipe data from being rendered.
+    };
+  }, [queryClient]);
+
   useEffect(() => {
     dispatch(
       setBreadcrumbs([
