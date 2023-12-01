d1a9b0e2bb488f1ae4ea603aed5b96f7197901ce
Jason Gin <jasongin88@gmail.com>
2023-12-01 14:23:08 -0500

    feat: use tx for multi query fns in recipe model

--- backend/src/helpers/Recipe.helper.js
+++ backend/src/helpers/Recipe.helper.js
@@ -10,38 +10,22 @@ import { pgpHelpers } from './utils/pgpHelpers.js';
 const pgp = pgPromise({ capSQL: true });
 
 const queries = {
-    recipeIngredientsQuery: `coalesce(${nestSelectQuery(
+    selectRecipeIngredientsQuery: `coalesce(${nestSelectQuery(
         'array_to_json(array_agg(x))',
         `SELECT i.ingredient_id,ingredient_name,amount,default_measurement,g_ml AS ml_for_100g,${nutrientsSelectQuery},${additionalMeasurementsQuery} 
          FROM recipes.ingredient AS i
          INNER JOIN recipes.recipe_ingredient as ri ON i.ingredient_id = ri.ingredient_id AND r.recipe_id = ri.recipe_id`
     )}, '[]'::json) AS ingredients`,
-    recipeToolsQuery: `coalesce(${nestSelectQuery(
+    selectRecipeToolsQuery: `coalesce(${nestSelectQuery(
         'array_to_json(array_agg(x))',
         `SELECT t.tool_id,tool_name 
          FROM recipes.tool as t 
          INNER JOIN recipes.recipe_tool as rt ON t.tool_id = rt.tool_id AND r.recipe_id = rt.recipe_id`
     )}, '[]'::json) AS tools`,
-    recipeReviewsQuery: `coalesce(${nestSelectQuery(
+    selectRecipeReviewsQuery: `coalesce(${nestSelectQuery(
         'array_to_json(array_agg(x))',
         `${reviewHelpers.getReviewsQuery} WHERE r.recipe_id = rr.recipe_id`
     )}, '[]'::json) AS reviews`,
-};
-
-export const recipeHelpers = {
-    validateInfo: (info) =>
-        typeof info.recipeName === 'string' &&
-        info.recipeName &&
-        Array.isArray(info.tools) &&
-        Array.isArray(info.ingredients) &&
-        info.ingredients.length &&
-        typeof info.diet === 'string' &&
-        typeof info.servings === 'string' &&
-        typeof info.time === 'string' &&
-        Array.isArray(info.instructions) &&
-        info.instructions,
-    getRecipesQuery: `SELECT r.recipe_id,recipe_name,time,diet,servings,instructions,${queries.recipeIngredientsQuery},${queries.recipeToolsQuery},${queries.recipeReviewsQuery} 
-                      FROM recipes.recipe as r`,
     insertIngredientsQuery: (ingredients, recipeId) =>
         pgp.helpers.insert(
             ingredients.map((ingr) => ({
@@ -67,4 +51,25 @@ export const recipeHelpers = {
             pgpHelpers.columns(['tool_id', 'recipe_id']),
             pgpHelpers.table('recipe_tool', 'recipes')
         ),
+};
+
+export const recipeHelpers = {
+    validateInfo: (info) =>
+        typeof info.recipeName === 'string' &&
+        info.recipeName &&
+        Array.isArray(info.tools) &&
+        Array.isArray(info.ingredients) &&
+        info.ingredients.length &&
+        typeof info.diet === 'string' &&
+        typeof info.servings === 'string' &&
+        typeof info.time === 'string' &&
+        Array.isArray(info.instructions) &&
+        info.instructions,
+    getRecipesQuery: `SELECT r.recipe_id,recipe_name,time,diet,servings,instructions,${queries.selectRecipeIngredientsQuery},${queries.selectRecipeToolsQuery},${queries.selectRecipeReviewsQuery} 
+                      FROM recipes.recipe as r`,
+    insertToolsAndIngredients: async (task, recipeId, ingredients, tools) => {
+        await task.none(queries.insertIngredientsQuery(ingredients, recipeId));
+        tools.length &&
+            (await task.none(queries.insertToolsQuery(tools, recipeId)));
+    },
 };
--- backend/src/models/Recipe.model.js
+++ backend/src/models/Recipe.model.js
@@ -1,8 +1,5 @@
-import pgPromise from 'pg-promise';
 import { recipeHelpers } from '../helpers/Recipe.helper.js';
 import { adminDb, userDB } from '../configs/db.config.js';
-
-const pgp = pgPromise({ capSQL: true });
 
 export const recipeModel = {
     getRecipes: async () =>
@@ -13,52 +10,57 @@ export const recipeModel = {
             [recipeId]
         ),
     createRecipe: async (info) => {
-        const recipeQuery =
-            'INSERT INTO recipes.recipe(recipe_name, time, diet, servings, instructions) VALUES ($1, $2, $3, $4, $5) RETURNING recipe_id;';
-        const { recipe_id: recipeId } = await adminDb.one(recipeQuery, [
-            ...(info.recipeId ? [info.recipeId] : []),
-            info.recipeName,
-            info.time,
-            info.diet,
-            info.servings,
-            info.instructions,
-        ]);
-        await adminDb.none(
-            recipeHelpers.insertIngredientsQuery(info.ingredients, recipeId)
-        );
-        info.tools.length &&
-            (await adminDb.none(
-                recipeHelpers.insertToolsQuery(info.tools, recipeId)
-            ));
+        await adminDb.tx(async (t) => {
+            const { recipe_id: recipeId } = await t.one(
+                'INSERT INTO recipes.recipe(recipe_name, time, diet, servings, instructions) VALUES ($1, $2, $3, $4, $5) RETURNING recipe_id;',
+                [
+                    ...(info.recipeId ? [info.recipeId] : []),
+                    info.recipeName,
+                    info.time,
+                    info.diet,
+                    info.servings,
+                    info.instructions,
+                ]
+            );
+            await recipeHelpers.insertToolsAndIngredients(
+                t,
+                recipeId,
+                info.ingredients,
+                info.tools
+            );
+        });
     },
     deleteRecipe: async (recipeId) =>
         await adminDb.none('DELETE FROM recipes.recipe WHERE recipe_id = $1;', [
             recipeId,
         ]),
     updateRecipe: async (recipeId, info) => {
-        await adminDb.none(
-            'UPDATE recipes.recipe SET (recipe_name, time, diet, servings, instructions) = ($2,$3,$4,$5,$6) WHERE recipe_id = $1',
-            [
-                info.recipeId,
-                info.recipeName,
-                info.time,
-                info.diet,
-                info.servings,
-                info.instructions,
-            ]
-        );
-        await adminDb.multi(
-            'DELETE FROM recipes.recipe_ingredient WHERE recipe_id = $1; ' +
-                recipeHelpers.insertIngredientsQuery(
-                    info.ingredients,
-                    recipeId
-                ),
-            [recipeId]
-        );
-        await adminDb.multi(
-            'DELETE FROM recipes.recipe_tool WHERE recipe_id = $1; ' +
-                recipeHelpers.insertToolsQuery(info.tools, recipeId),
-            [recipeId]
-        );
+        await adminDb.tx(async (t) => {
+            await t.none(
+                'UPDATE recipes.recipe SET (recipe_name, time, diet, servings, instructions) = ($2,$3,$4,$5,$6) WHERE recipe_id = $1',
+                [
+                    info.recipeId,
+                    info.recipeName,
+                    info.time,
+                    info.diet,
+                    info.servings,
+                    info.instructions,
+                ]
+            );
+            await t.none(
+                'DELETE FROM recipes.recipe_ingredient WHERE recipe_id = $1;',
+                [recipeId]
+            );
+            await t.none(
+                'DELETE FROM recipes.recipe_ingredient WHERE recipe_id = $1;',
+                [recipeId]
+            );
+            await recipeHelpers.insertToolsAndIngredients(
+                t,
+                recipeId,
+                info.ingredients,
+                info.tools
+            );
+        });
     },
 };
