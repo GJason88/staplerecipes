cd37cd9052e1d447c939fa142088c388c76ad191
Jason Gin <jasongin88@gmail.com>
2024-01-03 19:02:00 -0500

    feat: add coming soon routes

--- frontend/src/pages/Info.tsx
+++ frontend/src/pages/Info.tsx
@@ -1,14 +1,20 @@
 import { useDispatch } from 'react-redux';
 import { setActiveRoute } from '../layouts/layoutReducer';
-import IngredientsPage from '../features/ingredients/IngredientsPage';
 import { useEffect } from 'react';
 import { publicRoutes } from '../data/constants';
+import { Box, Typography } from '@mui/material';
 
-export default function Ingredients() {
+export default function Info() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(setActiveRoute(publicRoutes.info));
   }, [dispatch]);
 
-  return <IngredientsPage />;
+  return (
+    <Box display='flex' fontSize='48px' width='100%' justifyContent='center'>
+      <Typography fontSize='64px' fontStyle='italic'>
+        Coming Soon
+      </Typography>
+    </Box>
+  );
 }
---
+++ frontend/src/pages/Shop.tsx
@@ -0,0 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { setActiveRoute } from '../layouts/layoutReducer';
+import { useEffect } from 'react';
+import { publicRoutes } from '../data/constants';
+import { Box, Typography } from '@mui/material';
+
+export default function Shop() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setActiveRoute(publicRoutes.shop));
+  }, [dispatch]);
+
+  return (
+    <Box display='flex' fontSize='48px' width='100%' justifyContent='center'>
+      <Typography fontSize='64px' fontStyle='italic'>
+        Coming Soon
+      </Typography>
+    </Box>
+  );
+}
