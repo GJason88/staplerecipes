1f90189cf83b8f9199ed1105aeb294bd0eafcf9e
Jason Gin <jasongin88@gmail.com>
2024-02-07 21:58:47 -0500

    feat: update styling for info

--- frontend/src/features/info/styledComponents.tsx
+++ frontend/src/features/info/styledComponents.tsx
@@ -2,19 +2,17 @@ import { Box, styled } from '@mui/material';
 
 export const InfoBox = styled(Box)(() => ({
   display: 'flex',
-  height: '132px',
+  height: '96px',
   alignItems: 'center',
   justifyContent: 'center',
   borderRadius: '16px',
+  border: '1px solid rgba(0,0,0,0.25)',
   fontSize: '36px',
   fontWeight: 'bold',
   color: '#AB5353',
-  backgroundSize: 'cover',
-  backgroundColor: 'red',
-  boxShadow: 'inset 0 0 0 1000px rgba(249, 178, 98, 0.8), 0px 0px 0px 0px rgba(0, 0, 0, 0.25)',
   transition: '0.3s',
   ':hover': {
-    boxShadow: 'inset 0 0 0 1000px rgba(249, 178, 98, 0.6), 0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+    boxShadow: 'inset 0 0 0 1000px rgba(249, 178, 98, 0.4)',
     cursor: 'pointer',
   },
 }));
